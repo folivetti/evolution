ad3e95bddbaa4c8568c91982d8d06116b88874d5
Fabricio Olivetti <fabricio.olivetti@gmail.com>
2021-10-10 10:52:27 -0300

    first working version

--- src/Control/Evolution.hs
+++ src/Control/Evolution.hs
@@ -1,56 +1,170 @@
-module Control.Evolution
-    ( 
-    ) where
+module Control.Evolution where
 
-type Rnd a = a
-type Population a = [a]
-  {-
+import Control.Monad.State.Strict
+import Control.Monad.Reader
+import System.Random
+import Data.Monoid (Sum(..))
+import Data.Vector (Vector(..), (!))
+import qualified Data.Vector as V
+import Control.Scheduler                 (traverseConcurrently, Comp(..))
+import Control.DeepSeq                   (force)
+
+type Rnd a = StateT StdGen IO a
+
+type Population a = Vector a
+
 class Ord a => Solution a where
-  _evaluate :: a -> Rnd a
+  _evaluate   :: a -> Rnd a
+  _getFitness :: a -> Double
+  _isFeasible :: a -> Bool
 
-class (Show c, Read c) => Crossover c where
-  _cx :: Solution a => c -> [a] -> Rnd a
-    
-class (Show m, Read m) => Mutation m where
-  _mutate :: Solution a => m -> a -> Rnd a
-
-class (Show r, Read r) => Reproduction r where
-  _reproduce :: Solution a => r -> Population a -> Population a -> Rnd (Population a)
-
-class (Show s, Read s) => Selection s where
-  _select :: Solution a => s -> Population a -> Rnd [a]
-
-class SolutionBuilder sb where
-  _createSolution :: Solution a => sb -> Rnd a
--}
+data Interpreter a = Funs { _cx        :: Crossover -> [a] -> Rnd a
+                          , _mut       :: Mutation -> a -> Rnd a
+                          , _reproduce :: Reproduction -> Population a -> Population a -> Rnd (Population a)
+                          , _select    :: Selection -> Population a -> Rnd a
+                          , _filter    :: Predicate -> Population a -> Population a
+                          , _create    :: Rnd a
+                          } 
 
 data Reproduction = Generational 
                   | KeepBest 
                   | ReplaceWorst 
                   | Probabilistic Selection
+                  | CustomReproduction1
+                  | CustomReproduction2
                   deriving (Show, Read)
 
 data Selection = Tournament Int 
                | RouletteWheel 
                | SUS
+               | CustomSelection1
+               | CustomSelection2
                deriving (Show, Read)
 
 data Crossover = OnePoint 
                | NPoints Int
+               | CustomCX1
+               | CustomCX2
                deriving (Show, Read)
 
 data Mutation = SwapTree 
               | InsertVar
+              | CustomMut1
+              | CustomMut2
               deriving (Show, Read)
 
 data Predicate = Feasible 
                | Infeasible
+               | All
                deriving (Show, Read)
 
 -- | DSL of an evolutionary process
-data Evolution = Reproduce Reproduction Evolution Evolution
-               | Cross Crossover Selection Evolution
-               | Mutate Mutation Evolution
-               | Filter Predicate Evolution
-               | End
-               deriving (Show, Read)
+data Evolution = Reproduce Reproduction Predicate EvoCycle Predicate EvoCycle
+  deriving (Show, Read)
+
+data EvoCycle = Cross Crossover Int Double Selection EvoCycle
+              | Mutate Mutation Double EvoCycle
+              | End
+              deriving (Show, Read)
+
+randomDbl :: StateT StdGen IO Double
+randomDbl = state (randomR (0.0, 1.0))
+
+evalCycle :: Solution a 
+          => EvoCycle
+          -> Population a
+          -> a
+          -> ReaderT (Interpreter a) (StateT StdGen IO) a
+evalCycle End pop p  = pure p
+evalCycle (Cross cx nParents pc sel evo) pop p = do
+  prob <- lift randomDbl
+  cxf  <- asks _cx
+  self <- asks _select
+  parents <- lift $ replicateM nParents (self sel pop)
+  child <- if prob < pc
+              then lift $ cxf cx parents >>= _evaluate
+              else pure $ head parents
+  evalCycle evo pop child
+evalCycle (Mutate mut pm evo) pop p = do
+  prob  <- lift randomDbl
+  mutf  <- asks _mut
+  child <- if prob < pm
+              then lift $ mutf mut p >>= _evaluate
+              else pure p
+  evalCycle evo pop child
+
+evalEvo :: Solution a 
+        => Evolution 
+        -> Population a 
+        -> [StdGen]
+        -> ReaderT (Interpreter a) (StateT StdGen IO) (Population a, [StdGen])
+evalEvo (Reproduce rep pred1 evo1 pred2 evo2) pop gs = do
+  config       <- ask
+  keeper       <- asks _filter
+  (pop1, gs1)  <- case evo1 of
+                    End -> pure (keeper pred1 pop, gs)
+                    _   -> runPar evo1 config gs (keeper pred1 pop)
+  (pop2, gs2)  <- case evo2 of
+                    End -> pure (keeper pred2 pop, gs1)
+                    _   -> runPar evo2 config gs1 (keeper pred2 pop)
+  rFun <- asks _reproduce
+  pop' <- lift $ rFun rep pop1 pop2
+  return (pop', gs2)
+    where
+      runPar evo cfg g pop' = do
+        response <- liftIO $ traverseConcurrently (ParN 0) (runCycle evo cfg pop') $ zip (V.toList pop') g
+        pure $ splitResponse response ([], [])
+
+      splitResponse [] (accP, accG)           = (V.fromList accP, accG)
+      splitResponse ((p, g):pgs) (accP, accG) = splitResponse pgs (p:accP, g:accG)
+
+      runCycle evo conf pop' (ix, g) = flip runStateT g $ runReaderT (evalCycle evo pop' ix) conf
+
+genEvolution :: Solution a
+             => Int 
+             -> Int 
+             -> (Population a -> IO ()) 
+             -> Evolution 
+             -> ReaderT (Interpreter a) (StateT StdGen IO) ([Double], a)
+genEvolution nGens nPop logger evo = do
+  createSol <- asks _create
+  pop0      <- lift $ V.replicateM nPop (createSol >>= _evaluate)
+  liftIO $ logger pop0
+  g         <- lift get
+  let gens = splitGensWithIndex nPop g
+  go nGens pop0 ([avgFit pop0], V.minimum pop0) gens
+    where
+      go 0 _   (avgs, best) gs = return (reverse avgs, best)
+      go n pop (avgs, best) gs = do (pop', gs') <- evalEvo evo pop gs
+                                    liftIO $ logger pop'
+                                    let avgs' = avgFit pop' : avgs
+                                        best' = getBest best pop'
+                                    go (n-1) pop' (avgs', best') gs'
+
+runEvolution :: Solution a 
+             => Int
+             -> Int
+             -> (Population a -> IO ())
+             -> Evolution
+             -> StdGen
+             -> Interpreter a
+             -> IO ([Double], a)
+runEvolution nGens nPop logger evo g = flip evalStateT g . runReaderT (genEvolution nGens nPop logger evo)
+
+
+splitGensWithIndex :: Int -> StdGen -> [StdGen]
+splitGensWithIndex n = map fst . take n . iterate (\(g1,g2) -> split g2) . split 
+
+avgFit :: Solution a => Vector a -> Double
+avgFit pop = getSum tot / fromIntegral (V.length pop)
+  where
+    tot = foldMap (Sum . _getFitness) pop
+
+getBest :: Solution a => a -> Vector a -> a
+getBest best pop
+  | V.null feasible && _isFeasible best = best
+  | V.null feasible                     = min best (V.minimum pop)
+  | _isFeasible best                    = min best (V.minimum feasible)
+  | otherwise                           = V.minimum feasible
+  where
+    feasible = V.filter _isFeasible pop
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,71 @@
+import Control.Evolution
+import Control.Monad.State.Strict
+import qualified Data.Vector as V
+import System.Random
+
+data Test = Test { _chromo   :: [Double]
+                 , _fit      :: Double
+                 , _feasible :: Double
+                 } deriving Show
+
+instance Eq Test where
+  t1 == t2 = _fit t1 == _fit t2
+instance Ord Test where
+  t1 <= t2 = _fit t1 <= _fit t2
+
+instance Solution Test where
+  _evaluate t = pure $ t{ _fit = sum (_chromo t) }
+  _getFitness = _fit 
+  _isFeasible = (==0) . _feasible
+
+toss :: StateT StdGen IO Bool
+toss = state random
+
+testCreate = do
+  b1 <- state random
+  b2 <- state random
+  pure $ Test [b1, b2] 0.0 0.0
+
+testCX _ [p1,p2] = do
+  liftIO $ putStrLn "cx"
+  t <- toss
+  let cp1 = _chromo p1
+      cp2 = _chromo p2 
+  if t 
+     then pure (Test [cp1 !! 0, cp2 !! 1] 0.0 0.0)
+     else pure (Test [cp2 !! 0, cp1 !! 1] 0.0 0.0)
+testCX _ _ = testCreate
+
+testMut _ p = do
+  let cp = _chromo p
+  liftIO $ putStrLn "mutate"
+  t <- toss
+  r <- state random
+  if t
+    then pure (Test [cp !! 0, r] 0.0 0.0)
+    else pure (Test [r, cp !! 1] 0.0 0.0)
+
+testSelect _ pop = do
+  let n = V.length pop
+  liftIO $ putStrLn "select"
+  ix <- state (randomR (0, n-1))
+  pure $ pop V.! ix
+
+testReproduce _ p1 p2 = do
+  liftIO $ putStrLn "reproduce"
+  pure p2
+
+interpret :: Interpreter Test
+interpret = Funs testCX testMut testReproduce testSelect (\_ xs -> xs) testCreate
+
+evo = Reproduce Generational 
+        All End
+        All (Cross OnePoint 2 0.3 (Tournament 2) 
+               (Mutate SwapTree 0.7 End)
+            )
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  g <- getStdGen 
+  (xs, x) <- runEvolution 10 10 (\_ -> return ()) evo g interpret 
+  print x
