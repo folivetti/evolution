59b47ab7663f7b63ff82c72e307fdeb2eb0d7931
folivetti <folivetti@ufabc.edu.br>
2022-08-17 14:42:12 +0200

    bug fix in selection

--- src/Control/Evolution.hs
+++ src/Control/Evolution.hs
@@ -370,19 +370,19 @@ data Selection = Tournament Int
 select :: (Ord a, Solution a) => Selection -> Population a -> Rnd a
 select _ pop | V.null pop = error "empty population in selection"
 select (Tournament n) pop = do
-  (ix:ixs) <- replicateM n (randomInt (0, V.length pop - 1))
+  ixs <- replicateM n (randomInt (0, V.length pop - 1))
   pure $ minimumBy (compare `on` id) $ map (pop V.!) ixs
 -- TODO: rewrite this ugly thing
 select (FitShare sigma n) pop = do
-  (ix:ixs) <- replicateM n (randomInt (0, V.length pop - 1))
+  ixs <- replicateM n (randomInt (0, V.length pop - 1))
   let nPop = V.length pop
       distMtx = array ((0,0), (nPop-1, nPop-1)) [( (i,j), if i > j then distMtx A.! (j,i) else _distance (pop V.! i) (pop V.! j)) | i <- [0 .. nPop-1], j <- [0 .. nPop - 1]]
       sumDist = array (0, nPop-1) [ (i, 1.0 - sum [distMtx A.! (i,j) | j <- [0..nPop-1], i/=j] / fromIntegral nPop) | i <- [0..nPop-1]]
       minIx = minimumBy (compare `on` (\ix -> (head . _getFitness) (pop V.! ix) * (sumDist A.! ix))) ixs
   pure $ pop V.! minIx
 select (CrowdingTournament n) pop = do
-  (ix:ixs) <- replicateM n (randomInt (0, V.length pop - 1))
-  let pop'  = V.fromList (pop V.! ix : map (pop V.!) ixs)
+  ixs <- replicateM n (randomInt (0, V.length pop - 1))
+  let pop'  = V.fromList (map (pop V.!) ixs)
       (i:_) = crowdingDistance pop' $ head $ fastNondominatedSort pop'
   pure $ pop' V.! i
 select RouletteWheel pop = do
