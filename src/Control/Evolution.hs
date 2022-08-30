1825d0a761704b32179614dd5cf7d9d501ed1760
folivetti <folivetti@ufabc.edu.br>
2022-08-30 13:52:31 +0200

    debug

--- src/Control/Evolution.hs
+++ src/Control/Evolution.hs
@@ -383,7 +383,6 @@ select (FitShare sigma n) pop = do
                       ]
       sumDist = array (0, nPop-1) [ (i, sum [distMtx A.! (i,j) | j <- [0..nPop-1], i/=j] / fromIntegral (nPop - 1)) | i <- [0..nPop-1]]
       minIx = minimumBy (compare `on` (\ix -> (head . _getFitness) (pop V.! ix) * sigma * (1 + sumDist A.! ix))) ixs
-  liftIO $ print sumDist
   pure $ pop V.! minIx
 select (CrowdingTournament n) pop = do
   ixs <- replicateM n (randomInt (0, V.length pop - 1))
