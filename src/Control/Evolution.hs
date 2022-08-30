3de00eccecf8b37dfeeffb276372a5ae61425ff3
folivetti <folivetti@ufabc.edu.br>
2022-08-30 12:55:36 +0200

    debug

--- src/Control/Evolution.hs
+++ src/Control/Evolution.hs
@@ -371,6 +371,7 @@ select :: (Ord a, Solution a) => Selection -> Population a -> Rnd a
 select _ pop | V.null pop = error "empty population in selection"
 select (Tournament n) pop = do
   ixs <- replicateM n (randomInt (0, V.length pop - 1))
+  liftIO $ print $ minimumBy (compare `on` (\ix -> pop V.! ix)) ixs
   pure $ minimumBy (compare `on` id) $ map (pop V.!) ixs
 -- TODO: rewrite this ugly thing
 select (FitShare sigma n) pop = do
