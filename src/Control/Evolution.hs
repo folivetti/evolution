404ba82a729984f893e4ad436f772ce74f61635e
folivetti <fabricio.olivetti@gmail.com>
2024-08-09 13:28:13 -0300

    runtime control

--- src/Control/Evolution.hs
+++ src/Control/Evolution.hs
@@ -304,7 +304,7 @@ reproduce _ [] = error "reproduction must be applied to nonempty population"
 reproduce r _  = error $ "unsupported reproduction: " <> show r
 
 fastNondominatedSort :: Solution a => Population a -> [[Int]]
-fastNondominatedSort pop = traceShow (fstFront, nDoms) $ go [fstFront] dominationList nDoms
+fastNondominatedSort pop = traceShow (fstFront, dominationList) $ go [fstFront] dominationList nDoms
   where
     (fstFront, nDoms) = first M.keys                   -- returns only the keys of non-dominated and the map of the dominated
                       $ M.partition (==0) . M.fromList -- partition the list to those non-dominated and dominated
